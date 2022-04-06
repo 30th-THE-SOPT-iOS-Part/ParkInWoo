422081be94a3bc7c0e599486eabb24a371f0366d
inu <inwoo7233@naver.com>
2022-04-06 13:54:35 +0900

    chore: 프로젝트 생성

---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Inwoo Park on 2022/04/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
