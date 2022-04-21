95177e56e1d335d8e838f7c3945a3732832b6470
inuinseoul <inwoo7233@naver.com>
2022-04-21 22:10:05 +0900

    feat: Reels 터치시 색상변경 기능 추가

--- InstagramClone/InstagramClone/Presentation/TabBar/TabBarController.swift
+++ InstagramClone/InstagramClone/Presentation/TabBar/TabBarController.swift
@@ -9,49 +9,77 @@ import UIKit
 
 final class TabBarController: UITabBarController {
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        self.tabBar.backgroundColor = .white
-        self.configureTabs()
-    }
-    
-    private func configureTabs() {
+    private lazy var homeViewController: HomeViewController = {
         let home = HomeViewController()
         home.tabBarItem = UITabBarItem(
             title: .none,
-            image: .home?.withRenderingMode(.alwaysOriginal),
-            selectedImage: .homeSelected?.withRenderingMode(.alwaysOriginal)
+            image: .home?.withRenderingMode(.alwaysTemplate),
+            selectedImage: .homeSelected?.withRenderingMode(.alwaysTemplate)
         )
-        
+        return home
+    }()
+    
+    private lazy var searchViewController: SearchViewController = {
         let search = SearchViewController()
         search.tabBarItem = UITabBarItem(
             title: .none,
-            image: .search?.withRenderingMode(.alwaysOriginal),
-            selectedImage: .searchSelected?.withRenderingMode(.alwaysOriginal)
+            image: .search?.withRenderingMode(.alwaysTemplate),
+            selectedImage: .searchSelected?.withRenderingMode(.alwaysTemplate)
         )
-        
+        return search
+    }()
+    
+    private lazy var reelsViewController: ReelsViewController = {
         let reels = ReelsViewController()
         reels.tabBarItem = UITabBarItem(
             title: .none,
-            image: .reels?.withRenderingMode(.alwaysOriginal),
-            selectedImage: .reelsSelected?.withRenderingMode(.alwaysOriginal)
+            image: .reels?.withRenderingMode(.alwaysTemplate),
+            selectedImage: .reelsSelected?.withRenderingMode(.alwaysTemplate)
         )
-        
+        return reels
+    }()
+    
+    private lazy var shopViewController: ShopViewController = {
         let shop = ShopViewController()
         shop.tabBarItem = UITabBarItem(
             title: .none,
-            image: .shop?.withRenderingMode(.alwaysOriginal),
-            selectedImage: .shopSelected?.withRenderingMode(.alwaysOriginal)
+            image: .shop?.withRenderingMode(.alwaysTemplate),
+            selectedImage: .shopSelected?.withRenderingMode(.alwaysTemplate)
         )
-        
+        return shop
+    }()
+    
+    private lazy var profileViewController: ProfileViewController = {
         let profile = ProfileViewController()
         profile.tabBarItem = UITabBarItem(
             title: .none,
             image: .profile?.withRenderingMode(.alwaysOriginal),
             selectedImage: .profileSelected?.withRenderingMode(.alwaysOriginal)
         )
-        
-        self.viewControllers = [home, search, reels, shop, profile]
+        return profile
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.delegate = self
+        self.tabBar.tintColor = .black
+        self.tabBar.unselectedItemTintColor = .black
+        self.tabBar.backgroundColor = .white
+        self.viewControllers = [homeViewController, searchViewController, reelsViewController, shopViewController, profileViewController]
     }
     
+}
+
+extension TabBarController: UITabBarControllerDelegate {
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if viewController is ReelsViewController {
+            self.tabBar.tintColor = .white
+            self.tabBar.unselectedItemTintColor = .white
+            self.tabBar.backgroundColor = .black
+        } else {
+            self.tabBar.tintColor = .black
+            self.tabBar.unselectedItemTintColor = .black
+            self.tabBar.backgroundColor = .white
+        }
+    }
 }
